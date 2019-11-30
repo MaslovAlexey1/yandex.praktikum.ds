476939a357c21a36c9e0811712d7532f7db1ac93
Alexey Maslov <alexeymaslov@Alexeys-MacBook-Pro.local>
2019-11-30 13:26:20 +0300

    Sprint 5 study

---
+++ Sprint 5/r2_maximizetion.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+import joblib
+from sklearn.metrics import mean_absolute_error
+
+df = pd.read_csv('/Users/alexeymaslov/Git/github/praktikum/Sprint 5/datasets/flights_preprocessed.csv')
+features = df.drop(columns=['Arrival Delay'], axis=1)
+target = df['Arrival Delay']
+
+features_train, features_valid, target_train, target_valid = train_test_split(features, target, test_size=0.25, random_state=12345)
+
+
+model = LinearRegression()
+model.fit(features_train, target_train)
+print(model.score(features_valid, target_valid))
+
+for min_samples_leaf in range(2, 20, 1):
+    model = RandomForestRegressor(n_estimators=20, max_depth=12, random_state=12345, min_samples_leaf=min_samples_leaf)
+    model.fit(features_train, target_train)
+    print('min_samples_leaf=', min_samples_leaf, '| mae=', mean_absolute_error(features_valid, target_valid))
+
+# model = RandomForestRegressor(n_estimators=100, max_depth=12, random_state=12345, min_samples_leaf=7)
+# model.fit(features_train, target_train)
+# print(model.score(features_valid, target_valid))
+
+joblib.dump(model, 
+    '/Users/alexeymaslov/Git/github/praktikum/Sprint 5/best_model.joblib')
